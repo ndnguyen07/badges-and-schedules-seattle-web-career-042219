ed29cf02ae4cb449f33f17fa2ce5529d314f134b
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-17 07:00:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_room(attendees)
+  attendees.each_with_index.collect do |attendee, index + 1|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end
+end
+
